486c1de4bc5f25c0d32fa8be4d354c5601c779c2
Marcelo da Silva <marcelosilva.mdc@gmail.com>
2020-05-03 16:58:20 -0300

    global style ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Marcelo</h1>;
+  return (
+    <>
+      <h1>Marcelo</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
